54b6d74074ffa5a4c3a0faa48c21f82255e7e4c1
IceF0rEver <arnaudorban.pro@gmail.com>
2025-03-03 19:36:46 +0100

    feat: user auth update

---
+++ finances_app_backend/app/Http/Controllers/UserController.php
@@ -0,0 +1,92 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Http\Controllers\Controller;
+use Illuminate\Http\Request;
+use Illuminate\Http\Response;
+use App\Models\User;
+use Illuminate\Validation\Rules;
+use Illuminate\Validation\Rule;
+use Illuminate\Support\Facades\Auth;
+use Illuminate\Support\Facades\Hash;
+
+
+
+class UserController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     */
+    public function index()
+    {
+        //
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     */
+    public function store(Request $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     */
+    public function show(string $id)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     */
+    public function edit(string $id)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     */
+
+    public function update(Request $request) : Response
+    {
+        $user = Auth::user();
+
+        $request->validate([
+            'name' => ['required', 'string', 'max:255'],
+            'email' => ['required', 'string', 'lowercase', 'email', 'max:255', Rule::unique('users', 'email')->ignore($user->id)],
+            'password' => ['nullable', 'confirmed', Rules\Password::defaults()],
+        ]);
+        $user->name = $request->name;
+        $user->email = $request->email;
+    
+        if ($request->filled('password')) {
+            $user->password = Hash::make($request->password);
+        }
+    
+        $user->save();
+    
+        return response()->noContent();
+    }
+    
+    
+
+    /**
+     * Remove the specified resource from storage.
+     */
+    public function destroy(string $id)
+    {
+        //
+    }
+}
--- finances_app_backend/routes/api.php
+++ finances_app_backend/routes/api.php
@@ -4,6 +4,8 @@ use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\Auth\AuthenticatedSessionController;
 use App\Http\Controllers\Auth\RegisteredUserController;
+use App\Http\Controllers\UserController;
+
 
 Route::middleware(['auth:sanctum'])->group(function () {
 
@@ -11,6 +13,18 @@ Route::middleware(['auth:sanctum'])->group(function () {
         return $request->user();
     });
 
+    Route::patch('/user/update', [UserController::class, 'update'])
+    ->name('update');
+
     Route::post('/logout', [AuthenticatedSessionController::class, 'destroy'])
     ->name('logout');
+});
+
+Route::middleware('guest')->group(function () {
+
+    Route::post('/register', [RegisteredUserController::class, 'store'])
+    ->name('register');
+
+    Route::post('/login', [AuthenticatedSessionController::class, 'store'])
+    ->name('login');
 });
